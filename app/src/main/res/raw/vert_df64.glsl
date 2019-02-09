a68214cc6dbc332d3c7c7d69a5fb03ff5feb9fe6
Matt <mattjkelley710@gmail.com>
2019-02-08 23:47:57 -0500

    remove unnecessary code

--- app/src/main/res/raw/vert_df64.glsl
+++ app/src/main/res/raw/vert_df64.glsl
@@ -4,13 +4,6 @@ in vec4 vPos;
 
 void main() {
 
-    // vec2 xPos = vec2(vPos.x, 0.0);
-    // vec2 yPos = vec2(vPos.y, 0.0);
-
-    // C = scale*vPosition.xy + offset;
-
-    // xC = ds_add(ds_mul(xScale, xPos), xOffset);
-    // yC = ds_add(ds_mul(yScale, yPos), yOffset);
-
     gl_Position = vPos;
+
 }
