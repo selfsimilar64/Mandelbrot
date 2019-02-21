65cedcfca50d7289aae92475ab1f591bb2b90dce
Matt <mattjkelley710@gmail.com>
2019-02-21 17:24:12 -0500

    directly pass in view coordinates and quad coordinates

--- app/src/main/res/raw/vert_tex.glsl
+++ app/src/main/res/raw/vert_tex.glsl
@@ -1,12 +1,14 @@
 #version 320 es
 
-in vec4 vPos;
+in vec4 viewCoords;
+in vec4 quadCoords;
+uniform vec2 scale;
+uniform vec2 offset;
 out vec2 texCoord;
-//uniform mat4 m;
 
 void main() {
 
-    texCoord = 0.5*(vPos.xy + vec2(1.0));
-    gl_Position = vPos;
+    texCoord = 0.5*(viewCoords.xy + vec2(1.0));
+    gl_Position = quadCoords;
 
 }
