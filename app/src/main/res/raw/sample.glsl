84340f75cce2eaca86c494f1e24e7bbcb69f0b76
Matt <mattjkelley710@gmail.com>
2019-04-19 22:20:35 -0400

    replaced frag_tex

---
+++ app/src/main/res/raw/sample.glsl
@@ -0,0 +1,12 @@
+#version 320 es
+
+precision highp float;
+uniform sampler2D tex;
+in vec2 texCoord;
+out vec4 fragmentColor;
+
+void main() {
+
+    fragmentColor = texture(tex, texCoord);         // sample texture
+
+}
