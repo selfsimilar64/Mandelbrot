734839e932591c32229a6e2b826d82e484f1efa4
Matt <mattjkelley710@gmail.com>
2019-05-24 23:52:09 -0400

    pass key-value pair on parameter change, implemented dynamic resolution and precision

--- app/src/main/java/com/example/matt/gputest/SettingsFragment.kt
+++ app/src/main/java/com/example/matt/gputest/SettingsFragment.kt
@@ -1,18 +1,91 @@
 package com.example.matt.gputest
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import kotlinx.android.synthetic.main.settings_fragment.*
 
 
 class SettingsFragment : Fragment() {
 
+    private lateinit var callback : OnParamChangeListener
+    lateinit var initParams : Map<String, String>
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.settings_fragment, container, false)
+
+        val v = inflater.inflate(R.layout.settings_fragment, container, false)
+
+        val resolutionTabs = v.findViewById<TabLayout>(R.id.resolutionTabs)
+        resolutionTabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                val res = tab.text.toString()
+                callback.onSettingsParamsChanged("resolution", res)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+
+        })
+        resolutionTabs.getTabAt(
+            Resolution.valueOf(
+                savedInstanceState?.getString("resolution")
+                ?: initParams["resolution"] ?: ""
+            ).ordinal
+        )?.select()
+
+        val precisionTabs = v.findViewById<TabLayout>(R.id.precisionTabs)
+        precisionTabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                val p = tab.text.toString()
+                callback.onSettingsParamsChanged("precision", p)
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+
+            }
+
+        })
+        precisionTabs.getTabAt(
+            Precision.valueOf(
+                savedInstanceState?.getString("precision")
+                ?: initParams["precision"] ?: ""
+            ).ordinal
+        )?.select()
+
+        return v
+    }
+
+    fun setOnParamChangeListener(callback: OnParamChangeListener) {
+        this.callback = callback
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(
+                "resolution",
+                resolutionTabs.getTabAt(resolutionTabs.selectedTabPosition)?.text.toString()
+        )
+        outState.putString(
+                "precision",
+                precisionTabs.getTabAt(precisionTabs.selectedTabPosition)?.text.toString())
+        super.onSaveInstanceState(outState)
+    }
+
+    interface OnParamChangeListener {
+        fun onSettingsParamsChanged(key: String, value: String)
     }
 
 }
