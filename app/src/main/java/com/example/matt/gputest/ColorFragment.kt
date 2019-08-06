ab0e8231a883a267c218eee7b9a38af919ea5fc6
Matt <mattjkelley710@gmail.com>
2019-08-06 13:51:21 -0400

    added frequency/phase EditTexts

--- app/src/main/java/com/example/matt/gputest/ColorFragment.kt
+++ app/src/main/java/com/example/matt/gputest/ColorFragment.kt
@@ -1,14 +1,15 @@
 package com.example.matt.gputest
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
 
 
 class ColorFragment : Fragment() {
@@ -23,6 +24,24 @@ class ColorFragment : Fragment() {
 
         val v = inflater.inflate(R.layout.color_fragment, container, false)
 
+        val editListenerDone = {
+            editText: EditText, key: String, value: (w: TextView)->Any -> TextView.OnEditorActionListener {
+            w, actionId, event -> when (actionId) {
+            EditorInfo.IME_ACTION_DONE -> {
+                callback.onColorParamsChanged(key, value(w))
+                val imm = v.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, 0)
+                editText.clearFocus()
+                editText.isSelected = false
+                true
+            }
+            else -> {
+                Log.d("EQUATION FRAGMENT", "some other action")
+                false
+            }
+        }
+        }
+        }
 
         colorPaletteSpinner = v.findViewById(R.id.colorPaletteSpinner)
         colorPaletteSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
@@ -51,7 +70,19 @@ class ColorFragment : Fragment() {
         colorPaletteSpinner.setSelection(ColorPalette.all.keys.indexOf(
                 savedInstanceState?.getString("palette") ?: config.palette().name)
         )
-        
+
+        val frequencyEdit = v.findViewById<EditText>(R.id.frequencyEdit)
+        frequencyEdit.setText("%.5f".format(config.frequency()))
+        frequencyEdit.setOnEditorActionListener(editListenerDone(frequencyEdit, "frequency") {
+            it.text.toString().toDouble()
+        })
+
+        val phaseEdit = v.findViewById<EditText>(R.id.phaseEdit)
+        phaseEdit.setText("%.5f".format(config.phase()))
+        phaseEdit.setOnEditorActionListener(editListenerDone(frequencyEdit, "phase") {
+            it.text.toString().toDouble()
+        })
+
         return v
     }
 
