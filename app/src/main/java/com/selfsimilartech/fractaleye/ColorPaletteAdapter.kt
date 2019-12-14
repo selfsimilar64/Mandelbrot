0756ec4c65df02f1d1ebbaae8295795c4a5542ed
selfsimilar64 <mattjkelley710@gmail.com>
2019-12-14 18:53:30 -0500

    set palette preview image to pre-rendered bitmap

--- app/src/main/java/com/selfsimilartech/fractaleye/ColorPaletteAdapter.kt
+++ app/src/main/java/com/selfsimilartech/fractaleye/ColorPaletteAdapter.kt
@@ -1,22 +1,31 @@
 package com.selfsimilartech.fractaleye
 
+import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.drawable.GradientDrawable
-import android.view.ViewGroup
+import android.opengl.GLES30
+import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import android.support.v7.widget.RecyclerView
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.util.ArrayList
+import java.util.concurrent.Callable
+import java.util.concurrent.FutureTask
 
-/**
- * Created by Belal on 6/19/2017.
- */
+class ColorPaletteAdapter(
+        private val colorList: ArrayList<ColorPalette>
+) : RecyclerView.Adapter<ColorPaletteAdapter.ColorPaletteHolder>() {
 
-class ColorPaletteAdapter(val colorList: ArrayList<ColorPalette>) : RecyclerView.Adapter<ColorPaletteAdapter.ColorPaletteHolder>() {
+
 
     //this method is returning the view for each item in the list
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColorPaletteHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item, parent, false)
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.color_preview_item, parent, false)
         return ColorPaletteHolder(v)
     }
 
@@ -30,17 +39,31 @@ class ColorPaletteAdapter(val colorList: ArrayList<ColorPalette>) : RecyclerView
         return colorList.size
     }
 
-    //the class is hodling the list view
+    //the class is holding the list view
     class ColorPaletteHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         fun bindItems(palette: ColorPalette) {
-            val preview = itemView.findViewById<ImageView>(R.id.preview)
-            val name = itemView.findViewById<TextView>(R.id.name)
-            preview.setImageDrawable(GradientDrawable(
+
+            Log.d("COLOR PALETTE ADAPTER", "binding palette: ${palette.name}")
+
+//            fsv.f.palette = palette
+//            fsv.profile = RenderProfile.ICON
+//            fsv.requestRender()
+
+            val previewImage = itemView.findViewById<ImageView>(R.id.colorPreviewImage)
+            val previewPalette = itemView.findViewById<ImageView>(R.id.previewPalette)
+            val previewText = itemView.findViewById<TextView>(R.id.colorPreviewText)
+
+            previewPalette.setImageDrawable(GradientDrawable(
                     GradientDrawable.Orientation.LEFT_RIGHT,
                     palette.getColors(itemView.resources, palette.colors)
             ))
-            name.text = palette.name
+
+            previewText.text = palette.name
+
+            previewImage.setImageBitmap(palette.icon)
+            previewImage.scaleType = ImageView.ScaleType.CENTER_CROP
+
         }
     }
 
