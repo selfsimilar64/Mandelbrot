951a192e864948f9fe586edbf0927044541c16df
selfsimilar64 <mattjkelley710@gmail.com>
2020-02-09 02:39:20 -0500

    implemented basic notification functionality, added hideNavBar and fitToViewport settings, removed lateinit protocol

--- app/src/main/java/com/selfsimilartech/fractaleye/SettingsFragment.kt
+++ app/src/main/java/com/selfsimilartech/fractaleye/SettingsFragment.kt
@@ -1,45 +1,41 @@
 package com.selfsimilartech.fractaleye
 
 import android.Manifest
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.ValueAnimator
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.pm.PackageManager
-import android.graphics.Rect
+import android.os.Build
 import android.os.Bundle
-import android.support.design.widget.TabLayout
-import android.support.v4.app.ActivityCompat
-import android.support.v4.app.Fragment
-import android.support.v4.content.ContextCompat
-import android.support.v7.widget.CardView
+import com.google.android.material.tabs.TabLayout
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import androidx.core.content.ContextCompat
 import android.util.Log
-import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import kotlinx.android.synthetic.main.settings_fragment.*
-import java.util.*
 
 
 class SettingsFragment : Fragment() {
-    
-    lateinit var f : Fractal
-    lateinit var fsv : FractalSurfaceView
-    lateinit var sc : SettingsConfig
 
-    fun passArguments(f: Fractal, fsv: FractalSurfaceView, sc: SettingsConfig) {
-        this.f = f
-        this.fsv = fsv
-        this.sc = sc
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        val act = if (activity is MainActivity) activity as MainActivity else null
-        if (!this::f.isInitialized) f = act!!.f
-        if (!this::fsv.isInitialized) fsv = act!!.fsv
-        if (!this::sc.isInitialized) sc = act!!.sc
-        super.onCreate(savedInstanceState)
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "fractal notify"
+            val descriptionText = "remind about fractals"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel("Fractal Eye", name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                    (activity as MainActivity).getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View {
@@ -50,86 +46,39 @@ class SettingsFragment : Fragment() {
 
     override fun onViewCreated(v: View, savedInstanceState: Bundle?) {
 
-        val act = activity as? MainActivity ?: null
-
-        val cardHeaderListener = { cardBody: LinearLayout -> View.OnClickListener {
-
-            val card = cardBody.parent.parent as CardView
-            val initScrollY = settingsScroll.scrollY
-            val px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 15f, context?.resources?.displayMetrics).toInt()
-
-            val hStart : Int
-            val hEnd : Int
+        val act = activity as MainActivity
+        val f = act.f
+        val fsv = act.fsv
+        val sc = act.sc
 
 
-            if (cardBody.layoutParams.height == 0) {
-
-                // measure card body height
-                val matchParentMeasureSpec = View.MeasureSpec.makeMeasureSpec((cardBody.parent as View).width, View.MeasureSpec.EXACTLY)
-                val wrapContentMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED)
-                cardBody.measure(matchParentMeasureSpec, wrapContentMeasureSpec)
-
-                hStart = 0
-                hEnd = cardBody.measuredHeight
-                (it as Button).setCompoundDrawablesWithIntrinsicBounds(null, null, resources.getDrawable(R.drawable.expand, null), null)
-
-            }
-            else {
-
-                hStart = cardBody.height
-                hEnd = 0
-                (it as Button).setCompoundDrawablesWithIntrinsicBounds(null, null, resources.getDrawable(R.drawable.collapse, null), null)
-
-            }
-
-            val anim = ValueAnimator.ofInt(hStart, hEnd)
-            anim.addUpdateListener { animation ->
-                val intermediateHeight = animation?.animatedValue as Int
-                // Log.d("FRACTAL FRAGMENT", "intermediate height: $intermediateHeight")
-                cardBody.layoutParams.height = intermediateHeight
-                cardBody.requestLayout()
-
-                if (hStart == 0) {  // only scroll on card expansion
-
-                    // get scrollView dimensions and card coordinates in scrollView
-                    val scrollBounds = Rect()
-                    settingsScroll.getDrawingRect(scrollBounds)
-                    var top = 0f
-                    var temp = card as View
-                    while (temp !is ScrollView){
-                        top += (temp).y
-                        temp = temp.parent as View
-                    }
-                    val bottom = (top + card.height).toInt()
-
-                    if (scrollBounds.top > top - px) {  // card top is not visible
-                        settingsScroll.scrollY = ((1f - animation.animatedFraction)*initScrollY).toInt() + (animation.animatedFraction*(top - px)).toInt()  // scroll to top of card
-                    }
-                    else {  // card top is visible
-                        if (scrollBounds.bottom < bottom && card.height < settingsScroll.height - px) { // card bottom is not visible and there is space between card top and scrollView top
-                            settingsScroll.scrollY = bottom - settingsScroll.height  // scroll to show bottom
-                        }
-                    }
-                    // Log.d("FRACTAL FRAGMENT", "scroll to: ${(cardBody.parent.parent as CardView).y.toInt()}")
-                }
+//        restartActivityButton.setOnClickListener {
+//
+//            act.recreate()
+//
+//        }
+//        val nm = NotificationManagerCompat.from(context!!)
+//        createNotificationChannel()
+//        sendNotificationButton.setOnClickListener {
+//
+//            val builder = NotificationCompat.Builder(context!!, "Fractal Eye")
+//                    .setSmallIcon(R.drawable.mandelbrot_icon)
+//                    .setContentTitle("u need 2 make moar fractalz")
+//                    .setContentText("click here 4 fractalz")
+//                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+//            nm.notify(3, builder.build())
+//
+//        }
 
 
-            }
+        hideNavBarSwtich.isChecked = sc.hideNavBar
+        hideNavBarSwtich.setOnCheckedChangeListener { _, isChecked ->
 
-            // change cardBody height back to wrap_content after expansion
-            if (cardBody.layoutParams.height == 0) {
-                anim.addListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator?) {
-                        super.onAnimationEnd(animation)
-                        cardBody.layoutParams.height = LinearLayout.LayoutParams.WRAP_CONTENT
-                    }
-                })
-            }
+            sc.hideNavBar = isChecked
+            fsv.updateSystemUI()
 
-            anim.duration = 250
-            anim.start()
+        }
 
-        }}
 
 
         resolutionBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
@@ -172,7 +121,7 @@ class SettingsFragment : Fragment() {
                         ?: sc.displayParams
         displayParamsSwitch.setOnCheckedChangeListener { _, isChecked ->
             sc.displayParams = isChecked
-            act?.updateDisplayParams(settingsChanged = true)
+            act.updateDisplayParams(settingsChanged = true)
         }
 
 
@@ -188,15 +137,39 @@ class SettingsFragment : Fragment() {
         }
 
 
+        fitToViewportSwitch.isChecked = sc.fitToViewport
+        fitToViewportSwitch.setOnCheckedChangeListener { _, isChecked ->
+
+            sc.fitToViewport = isChecked
+            act.recalculateSurfaceViewLayout()
+
+        }
+
+
+
+//        showHintsSwitch.isChecked = sc.showHints
+//        showHintsSwitch.setOnCheckedChangeListener { _, isChecked ->
+//
+//            sc.showHints = isChecked
+//            act.updateHintVisibility()
+//
+//        }
+
+
         saveToFileButton.setOnClickListener {
-            if (fsv.r.isRendering) act?.showMessage(resources.getString(R.string.msg_save_wait))
+            if (fsv.r.isRendering) act.showMessage(resources.getString(R.string.msg_save_wait))
             else {
-                if (ContextCompat.checkSelfPermission(v.context, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(
-                            act!!,
-                            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
-                            WRITE_STORAGE_REQUEST_CODE)
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+                    if (ContextCompat.checkSelfPermission(v.context, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(
+                                act,
+                                arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                                WRITE_STORAGE_REQUEST_CODE)
+                    }
+                    else {
+                        fsv.renderProfile = RenderProfile.SAVE
+                        fsv.requestRender()
+                    }
                 }
                 else {
                     fsv.renderProfile = RenderProfile.SAVE
