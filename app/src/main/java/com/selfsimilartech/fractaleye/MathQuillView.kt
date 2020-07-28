2deb3c38bbec70d97ed94f7070c18b735f513997
selfsimilar64 <mattjkelley710@gmail.com>
2020-07-28 12:04:53 -0400

    added class for handling MathQuill inside WebView

---
+++ app/src/main/java/com/selfsimilartech/fractaleye/MathQuillView.kt
@@ -0,0 +1,48 @@
+package com.selfsimilartech.fractaleye
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.webkit.JavascriptInterface
+import android.webkit.ValueCallback
+import android.webkit.WebView
+
+class MathQuillView : WebView {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyle: Int) : super(context, attrs, defStyle)
+
+
+    fun enterExpr(expr: Expr) {
+
+        if (expr.isCmd) evaluateJavascript("javascript:mathField.cmd('${expr.str}')", null)
+        else evaluateJavascript("javascript:mathField.typedText('${expr.str}')", null)
+
+        if (expr.insertParens) evaluateJavascript("javascript:mathField.typedText('()')", null)
+
+        when {
+            expr.rightAfter -> enterKeystroke("Right")
+            expr.leftAfter -> enterKeystroke("Left")
+        }
+
+    }
+
+    fun enterKeystroke(ks: String) {
+
+        evaluateJavascript("javascript:mathField.keystroke('$ks')", null)
+
+    }
+
+    fun getLatex(onValueReceived: (String) -> Unit) {
+        evaluateJavascript("javascript:mathField.latex()") {
+            Log.e("MQ", "result: $it")
+            onValueReceived(it.substring(1..it.length-2))
+        }
+    }
+
+    fun setLatex(latex: String) {
+        evaluateJavascript("javascript:mathField.latex('$latex')", null)
+    }
+
+}
